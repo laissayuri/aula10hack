d1fc20fc95cd466dc0d348af72125160876a7b4d
natlb <natalia_lira1@hotmail.com>
2018-08-17 11:40:09 -0300

    java do renan

---
+++ modelo.js
@@ -0,0 +1,74 @@
+let sala = {
+    altura: 305,
+    largura: 460,
+    comprimento: 215,
+    corDasParedes: "azul claro",
+    moveis:[
+        sofa: {
+            cor: "marrom",
+            lugares: 3,
+            peso: 70
+        }
+        estante:{
+            material: "madeira",
+            cupim: true,
+            prateleiras:[
+                [
+                    {
+                        nome: "Cem Anos de Solidão",
+                        autor: "Gabriel García Marquez"
+                    },
+                    {
+                        nome: "Comédias para se ler na escola"
+                        autor: "Luiz Fernando Veríssimo"
+                    }
+                ],
+                [
+                    {
+                        nome: "O guia do mochileiro das galáxias",
+                        autor: "Douglas Adams"
+                    },
+                    {
+                        nome: "O restaurante no fim do universo",
+                        autor: "Douglas Adams"
+                    },
+                    {
+                        nome: "A vida, o universo e tudo mais",
+                        autor: "Douglas Adams"
+                    },
+                    {
+                        nome: "Até mais, e obrigado por todos os peixes",
+                        autor: "Douglas Adams"
+                    },
+                    {
+                        nome: "Praticamente inofensiva",
+                        autor: "Douglas Adams"
+                    }
+                ],
+                [
+                    {
+                        nome: "O Senhor dos Anéis: A Sociedade do Anel",
+                        autor: "J. R. R. Tolkien"
+                    },
+                    {
+                        nome: "O Senhor dos Anéis: As Duas Torres",
+                        autor: "J. R. R. Tolkien"
+                    },
+                    {
+                        nome: "O Senhor dos Anéis: O Retorno do Rei",
+                        autor: "J. R. R. Tolkien"
+                    },
+                    {
+                        nome: "O Hobbit",
+                        autor: "J. R. R. Tolkien"
+                    }
+                ]
+            ]
+        }
+    ],
+    luminaria: {
+        marca: "Luz Zia",
+        potencia: 80,
+        acesa: true
+    }
+}
